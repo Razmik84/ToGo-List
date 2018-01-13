af0802dcac3a4dae7a81d72884d5f9cff7f0fa4a
Razmik Bourtoutian <razmik1984@gmail.com>
2018-01-13 17:01:16 +0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <google-map></google-map>
+  </div>
+</template>
+
+<script>
+
+import GoogleMap from './components/GoogleMap.vue';
+
+export default {
+
+  components: {
+    'google-map': GoogleMap,
+  },
+
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #app {
+    background: #e6eaf2;
+  }
+</style>
